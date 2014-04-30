c3b15e2bb4d6c871718782c64c487ff101d7e345
Daniel M <dmana@flumotion.com>
2014-04-30 15:35:43 +0200

    Enable fade-in animations for playlists

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -2,6 +2,17 @@
 
 angular.module('pocFlaixfmApp', [
   'fmt'
-]);
+])
 
-window.fmtBaseUrl = 'http://bo.flaix.webtv.flumotion.com/';
+  .run(function ($window) {
+    $window.fmtBaseUrl = 'http://bo.flaix.webtv.flumotion.com/';
+
+    // setup fade animations
+    $(window).scroll(function () {
+      return $(".animate-in").not(".hidden").each(function (i, element) {
+        if ($(element).offset().top + 80 < ($(window).height() + $(window).scrollTop())) {
+          return $(element).removeClass('animate-in');
+        }
+      });
+    });
+  });
